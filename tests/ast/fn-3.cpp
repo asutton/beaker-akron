8b937489d020c6bcf99fe0ab7d04e3c82e5388ba
Andrew Sutton <andrew.n.sutton@gmail.com>
2016-12-28 09:17:33 -0500

    Add boostrap code to pass by reference.

--- tests/ast/fn-3.cpp
+++ tests/ast/fn-3.cpp
@@ -59,20 +59,19 @@ main(int argc, char* argv[])
 
   //Values
   expr& t = lb.get_true_expr();
-  expr& z = nb.make_int_expr(i32, 42);
-  expr& big = nb.make_int_expr(i1024, 5);
+  expr& z1 = nb.make_int_expr(i32, 42);
+  expr& z2 = nb.make_int_expr(i1024, 5);
 
-  // Some declarations.
-  decl* vars[] {
-    &cb.make_parm_decl(cb.get_name("a"), rb),
-    &cb.make_parm_decl(cb.get_name("b"), ri32),
-    &cb.make_parm_decl(cb.get_name("c"), ri1024),
-    &cb.make_parm_decl(cb.get_name("r"), i32)
-  };
-
-  // Passes a couple objects by reference, returns a copy.
+  // Passes a couple objects by reference, returns one by copy.
   typed_decl* d1;
   { // f1 : (bool&, int32&) -> i32
+    decl* vars[] {
+      &cb.make_parm_decl("a", rb),
+      &cb.make_parm_decl("b", ri32),
+      &cb.make_parm_decl("c", ri1024),
+      &cb.make_parm_decl("r", i32)
+    };
+
     decl_seq parms {vars[0], vars[1]};
     decl& ret = *vars[3];
     type& type = cb.get_fn_type(parms, ret);
@@ -85,28 +84,41 @@ main(int argc, char* argv[])
     mod.add_declaration(*d1);
   }
 
-  // // Generate a call expression.
-  // expr& f1 = cb.make_ref_expr(d1->get_type(), *d1);
-  // expr_seq args {
-  //   &cb.make_copy_init(z),
-  //   &cb.make_copy_init(t),
-  //   &cb.make_copy_init(big)
-  // };
-  // expr& call = cb.make_call_expr(i32, f1, args);
 
-  // // Generate the function definition (note that copy of the call).
-  // stmt_seq stmts {
-  //   &cb.make_ret_stmt(cb.make_copy_init(call))
-  // };
-  // stmt& def = cb.make_block_stmt(stmts);
+  // Build a main function that declares some variables to be passed
+  // by reference.
 
-  // // main : () -> int32
-  // decl_seq parms;
-  // decl& ret = *vars[3];
-  // type& type = cb.get_fn_type(parms, ret);
-  // decl& fn = cb.make_fn_decl(cb.get_name("main"), type, parms, ret, def);
-  // mod.add_declaration(fn);
-  // print(mod);
+  // Make some variables and their initializers.
+  decl_seq vars {
+    &cb.make_var_decl("b", b, cb.make_copy_init(t)),
+    &cb.make_var_decl("z1", i32, cb.make_copy_init(z1)),
+    &cb.make_var_decl("z2", i1024, cb.make_copy_init(z2)),
+  };
+
+  // Generate a call expression to f1.
+  expr& f1 = cb.make_ref_expr(d1->get_type(), *d1);
+  expr_seq args {
+    &cb.make_ref_init(cb.make_ref_expr(rb, vars[0])),
+    &cb.make_ref_init(cb.make_ref_expr(ri32, vars[1]))
+  };
+  expr& call = cb.make_call_expr(i32, f1, args);
+
+  stmt_seq stmts {
+    &cb.make_decl_stmt(vars[0]),
+    &cb.make_decl_stmt(vars[1]),
+    // &cb.make_decl_stmt(vars[2]),
+    &cb.make_ret_stmt(cb.make_copy_init(call)),
+  };
+  stmt& def = cb.make_block_stmt(stmts);
+
+  // Build a wrapper function.
+  decl_seq parms;
+  decl& ret = cb.make_parm_decl("ret", i32);
+  type& ftype = cb.get_fn_type(parms, ret);
+  decl& fn = cb.make_fn_decl("main", ftype, parms, ret, def);
+  mod.add_declaration(fn);
+
+  print(mod);
 
   // Emit LLVM.
   generator gen("out.ll");
