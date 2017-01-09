989cb7a3009a98f214bbe3f6ce4c143d2a759393
Andrew Sutton <andrew.n.sutton@gmail.com>
2017-01-09 13:39:28 -0500

    Update sys.int language.

--- beaker/sys.int/lang.cpp
+++ beaker/sys.int/lang.cpp
@@ -1,24 +1,22 @@
-// Copyright (c) 2015-2016 Andrew Sutton
+// Copyright (c) 2015-2017 Andrew Sutton
 // All rights reserved
 
 #include "lang.hpp"
-#include <beaker/numeric/construction/builder.hpp>
-#include <beaker/numeric/comparison/eq.hpp>
-#include <beaker/numeric/comparison/hash.hpp>
-#include <beaker/numeric/printing/print.hpp>
-#include <beaker/numeric/generation/gen.hpp>
-
-#include <cassert>
+#include "construction/builder.hpp"
+#include "comparison/eq.hpp"
+#include "comparison/hash.hpp"
+#include "printing/print.hpp"
+#include "generation/gen.hpp"
 
 
 namespace beaker {
-namespace numeric {
+namespace sys_int {
 
 /// Allocates a builder.
 static void* make_builder(module& m) { return new builder(m); }
 
 feature::feature()
-  : basic_feature<numeric_lang>(make_builder)
+  : basic_feature<sys_int_lang>(make_builder)
 {
   add_algorithm<eq_algo>();
   add_algorithm<hash_algo>();
@@ -26,5 +24,5 @@ feature::feature()
   add_algorithm<gen_algo>();
 }
 
-} // namespace numeric
+} // namespace sys_int
 } // namespace beaker
--- beaker/sys.int/lang.hpp
+++ beaker/sys.int/lang.hpp
@@ -1,24 +1,24 @@
-// Copyright (c) 2015-2016 Andrew Sutton
+// Copyright (c) 2015-2017 Andrew Sutton
 // All rights reserved
 
-#ifndef BEAKER_NUMERIC_LANG_HPP
-#define BEAKER_NUMERIC_LANG_HPP
+#ifndef BEAKER_SYS_INT_LANG_HPP
+#define BEAKER_SYS_INT_LANG_HPP
 
 #include <beaker/base/lang.hpp>
 
 
 namespace beaker {
-namespace numeric {
+namespace sys_int {
 
 /// A feature defines an extensible set of terms that can be used with the
 /// beaker language runtime. In particular, this exposes a core set of 
 /// algorithms and facilities needed to operate on those terms.
-struct feature : basic_feature<numeric_lang>
+struct feature : basic_feature<sys_int_lang>
 {
   feature();
 };
 
-} // namespace numeric
+} // namespace sys_int
 } // namespace beaker
 
 
